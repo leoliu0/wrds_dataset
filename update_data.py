6f1a71d2df6d5b9c780b61d48de2d1c6888c545a
Leo Liu <auleo.liu@gmail.com>
2019-05-27 13:21:38 +1000

    kld

--- update_data.py
+++ update_data.py
@@ -2,13 +2,11 @@ import pandas as pd
 from glob import glob
 import numpy as np
 
-
 def get_fyear(df):
     if df.datadate.month > 6:
         return df.datadate.year
     else:
         return df.datadate.year - 1
-
 
 # Process the link file and create daily link file
 print('process link file ... takes about 10 mins')
@@ -25,41 +23,69 @@ for gvkey, permno, linkdt, linkenddt in ccm.values:
         d += np.timedelta64('1', 'D')
 
 link_full = pd.DataFrame(link_full)
+link_full.columns = ['gvkey','permno','date']
+link_full['gvkey'] = pd.to_numeric(link_full.gvkey)
+print(f'writing link table')
+link_full.to_parquet('ccm_linktable.par')
 
 print('process funda .... takes about 5 mins')
-funda = pd.read_sas('funda.sas7bdat', encoding='latin1')
 
+comp_name = pd.read_sas('names.sas7bdat',encoding='latin1')
+comp_name = comp_name[['gvkey','sic','naics','year1','year2']]
+for col in comp_name.columns:
+    comp_name[col] = pd.to_numeric(comp_name[col])
+
+funda = pd.read_sas('funda.sas7bdat', encoding='latin1')
+funda['gvkey'] = pd.to_numeric(funda.gvkey)
+funda['cik'] = pd.to_numeric(funda.cik)
 funda['_fyear'] = funda.apply(get_fyear, axis=1)
 funda['fyear'] = funda['fyear'].fillna(funda._fyear)
 funda.drop('_fyear', axis=1)
 funda = funda[lambda df:(df.datafmt == 'STD') & (df.consol == 'C') & (df.indfmt == 'INDL')
               ][lambda df:df['at'].notnull()]
 
+funda = funda.merge(comp_name,on='gvkey')
+
+funda.columns = [x.lower() for x in funda.columns]
+
 ccm_linktable = pd.read_parquet('ccm_linktable.par')
 ccm = funda.merge(ccm_linktable.rename({'date': 'datadate'}, axis=1))
 fundaccm = funda.merge(ccm_linktable.rename(
     {'date': 'datadate'}, axis=1), how='left')
 
+print(f'writing ccm, total {len(ccm)} obs')
 ccm.to_parquet('ccm.par')
+print(f'writing fundaccm, total {len(fundaccm)} obs')
 fundaccm.to_parquet('fundaccm.par')
 
 print('process msf and dsf ... takes about 1 hour')
 msf = pd.read_sas('msf.sas7bdat',encoding='latin1')
 dsf = pd.read_sas('dsf.sas7bdat',encoding='latin1')
 
-msf['fyear'] = msf.DATE.map(get_fyear)
-msf['year'] = msf.DATE.map(lambda s:s.year)
-msf['month'] = msf.DATE.map(lambda s:s.month)
-msf = msf.merge(ccm_linktable.rename({'permno':'PERMNO','date':'DATE'},axis=1),how='left')
+msf.columns = [x.lower() for x in msf.columns]
+dsf.columns = [x.lower() for x in dsf.columns]
+
+msf['fyear'] = msf.date.map(lambda s:s.year if s.month>6 else s.year-1)
+msf['year'] = msf.date.map(lambda s:s.year)
+msf['month'] = msf.date.map(lambda s:s.month)
+msf = msf.merge(ccm_linktable,how='left')
 msf_ccm = msf[lambda df:df.gvkey.notnull()]
+print(f'writing msf_ccm, total {len(msf_ccm)} obs')
 msf_ccm.to_parquet('msf_ccm.par')
+print(f'writing msf, total {len(msf)} obs')
 msf.to_parquet('msf.par')
 
-dsf['fyear'] = dsf.DATE.map(get_fyear)
-dsf['year'] = dsf.DATE.map(lambda s:s.year)
-dsf['month'] = dsf.DATE.map(lambda s:s.month)
-dsf = dsf.merge(ccm_linktable.rename({'permno':'PERMNO','date':'DATE'},axis=1),how='left')
+dsf['fyear'] = dsf.date.map(lambda s:s.year if s.month>6 else s.year-1)
+dsf['year'] = dsf.date.map(lambda s:s.year)
+dsf['month'] = dsf.date.map(lambda s:s.month)
+dsf = dsf.merge(ccm_linktable,how='left')
 dsf_ccm = dsf[lambda df:df.gvkey.notnull()]
+print(f'writing dsf_ccm, total {len(dsf_ccm)} obs')
 dsf_ccm.to_parquet('dsf_ccm.par')
+print(f'writing dsf, total {len(dsf)} obs')
 dsf.to_parquet('dsf.par')
 
+# KLD data
+print('KLD data update')
+kld = pd.read_sas('history.sas7bdat', encoding='latin1')
+kld.to_parquet('kld.par')
--- update_data.sh
+++ update_data.sh
@@ -6,6 +6,7 @@ scp z3486253@wrds-cloud.wharton.upenn.edu:/wrds/comp/sasdata/naa/funda.sas7bdat 
 scp z3486253@wrds-cloud.wharton.upenn.edu:/wrds/crsp/sasdata/a_stock/msi.sas7bdat .
 scp z3486253@wrds-cloud.wharton.upenn.edu:/wrds/crsp/sasdata/a_stock/dsi.sas7bdat .
 scp z3486253@wrds-cloud.wharton.upenn.edu:/wrds/comp/sasdata/naa/names.sas7bdat .
+scp z3486253@wrds-cloud.wharton.upenn.edu:/wrds/kld/sasdata/history.sas7bdat .
 
 python3 update_data.py
 
